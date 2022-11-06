dcf19d6fdee418705e6fea332c2fa9f1fd234f73
Shivraj <shivrajofficial102@gmail.com>
2022-11-06 23:16:59 +0530

    Adding Paginate Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { React, useState, useEffect } from "react";
+import Pagination from "./Pagination/Pagination";
 import axios from "axios";
 import "./App.css";
 import Coin from "./Coin";
-
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [currentPage, setCurrrentPage] = useState(1);
+  const [recordsPerPage] = useState(10);
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   useEffect(() => {
@@ -23,6 +25,9 @@ function App() {
   const filteredCoins = coins.filter((coin) => {
     return coin.name.toLowerCase().includes(search.toLowerCase());
   });
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const nPages = Math.ceil(coins.length / recordsPerPage);
   return (
     <>
       <div className="coin-app">
@@ -37,20 +42,27 @@ function App() {
             />
           </form>
         </div>
-        {filteredCoins.map((coin) => {
-          return (
-            <Coin
-              key={coin.id}
-              name={coin.name}
-              image={coin.image}
-              symbol={coin.symbol}
-              marketcap={coin.market_cap}
-              price={coin.current_price}
-              priceChange={coin.price_change_percentage_24h}
-              volume={coin.total_volume}
-            />
-          );
-        })}
+        {filteredCoins
+          .slice(indexOfFirstRecord, indexOfLastRecord)
+          .map((coin) => {
+            return (
+              <Coin
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                marketcap={coin.market_cap}
+                price={coin.current_price}
+                priceChange={coin.price_change_percentage_24h}
+                volume={coin.total_volume}
+              />
+            );
+          })}
+        <Pagination
+          nPages={nPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrrentPage}
+        />
       </div>
     </>
   );
---
+++ src/Pagination/Pagination.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Pagination.css";
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+
+  const nextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(currentPage + 1);
+  };
+  const prevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        <li className="page-item">
+          <button className="page-link">
+            <a onClick={prevPage} href="#">
+              Previous
+            </a>
+          </button>
+        </li>
+        {pageNumbers.map((pgNumber) => (
+          <li
+            key={pgNumber}
+            className={`page-item ${currentPage === pgNumber ? "active" : ""} `}
+          >
+            {/* <a
+              onClick={() => setCurrentPage(pgNumber)}
+              className="page-link"
+              href="#"
+            >
+              {pgNumber}
+            </a> */}
+            <button className="page-link">
+              <a onClick={() => setCurrentPage(pgNumber)} href="#">
+                {pgNumber}
+              </a>
+            </button>
+          </li>
+        ))}
+        <li className="page-item">
+          <button className="page-link">
+            <a onClick={nextPage} href="#">
+              Next
+            </a>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
