974ec284795972677c930a7e34a6aee406543498
Shivraj <shivrajofficial102@gmail.com>
2022-11-28 22:39:18 +0530

    Search Pagination Component Fixed

--- src/Components/CoinsTable.jsx
+++ src/Components/CoinsTable.jsx
@@ -12,7 +12,6 @@ const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   const [currentPage, setCurrrentPage] = useState(1);
-  // const [loading , setLoading] = useState(init)
   const [recordsPerPage] = useState(10);
   const { currency, symbol } = CryptoState();
   const fetchCoins = async () => {
@@ -36,24 +35,72 @@ const CoinsTable = () => {
   const indexOfLastRecord = currentPage * recordsPerPage;
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   const nPages = Math.ceil(coins.length / recordsPerPage);
-  return (
-    <>
-      <div className="coin-app">
-        <div className="coin-search">
-          <h1 className="coin-text">Search Currency</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Search"
-              className="coin-input"
-              onChange={handleChange}
-            />
-          </form>
+  if (filteredCoins.length === coins.length) {
+    return (
+      <>
+        <div className="coin-app">
+          <div className="coin-search">
+            <h1 className="coin-text">Search Currency</h1>
+            <form>
+              <input
+                type="text"
+                placeholder="Search"
+                className="coin-input"
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+
+          <div className="cointable">
+            {filteredCoins
+              .slice(indexOfFirstRecord, indexOfLastRecord)
+              .map((coin) => {
+                return (
+                  <Link
+                    to={`/coins/${coin.id}`}
+                    element={<CoinPage />}
+                    key={coin.id}
+                  >
+                    <Coin
+                      name={coin.name}
+                      image={coin.image}
+                      symbol={coin.symbol}
+                      marketcap={coin.market_cap}
+                      price={coin.current_price}
+                      priceChange={coin.price_change_percentage_24h}
+                      volume={coin.total_volume}
+                      currency={symbol}
+                    />
+                  </Link>
+                );
+              })}
+          </div>
+          <Pagination
+            nPages={nPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrrentPage}
+          />
         </div>
-        <div className="cointable">
-          {filteredCoins
-            .slice(indexOfFirstRecord, indexOfLastRecord)
-            .map((coin) => {
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="coin-app">
+          <div className="coin-search">
+            <h1 className="coin-text">Search Currency</h1>
+            <form>
+              <input
+                type="text"
+                placeholder="Search"
+                className="coin-input"
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+
+          <div className="cointable">
+            {filteredCoins.map((coin) => {
               return (
                 <Link
                   to={`/coins/${coin.id}`}
@@ -73,15 +120,11 @@ const CoinsTable = () => {
                 </Link>
               );
             })}
+          </div>
         </div>
-        <Pagination
-          nPages={nPages}
-          currentPage={currentPage}
-          setCurrentPage={setCurrrentPage}
-        />
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default CoinsTable;
